b793a2acdd8e98021cc8ce8b0d692f87b127b63f
Harman <harmas7626@gmail.com>
2023-04-27 21:54:20 +0530

    spa

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,20 @@
+import styles from '../css/About.module.css'
+import png from '../assets/about.png'
 function About() {
     return (
-        <div>
-            <h1>Welcome to about page</h1>
+        <div className={styles.flex}>
+            <div className={styles.container}>
+                <h1 className={styles.title}>About <span className={styles.me}>me</span></h1>
+                <p className={styles.desc}>Hello! My name is Harmanpreet singh, and I am a MERN stack developer with [number] years of experience. My passion for web development started 4 months ago when I built my first website, and I haven't looked back since.<br />
+
+                    As a MERN stack developer, I am skilled in working with MongoDB, Express.js, React.js, and Node.js to build scalable and efficient web applications. I enjoy using my problem-solving skills to tackle complex coding challenges and finding elegant solutions that improve user experience.
+                    <br />
+
+                    In addition to my technical skills, I am a strong collaborator and communicator. I believe that clear communication is essential to the success of any project, and I enjoy working closely with my team to ensure that everyone is on the same page.
+                    <br />
+                    When I'm not coding, you can find me scrolling youtube and instagram, or volunteering with insert organization or cause. Thank you for taking the time to learn a little bit about me, and I look forward to the opportunity to work together on your next project.</p>
+            </div>
+           <div><img src={png}  className={styles.png}/> </div> 
         </div>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,34 @@
+import styles from '../css/contact.module.css'
 function Contact() {
     return (
-        <div>
-            <h1>Call me 7626851237</h1>
-        </div>
+        <div className="container">
+        <h3>Contact Form</h3>
+        <form action="#" name="contact_form">
+          <label htmlFor="first_name">First Name</label>
+          <input name="first_name" type="text" required="" placeholder="John" />
+          <br />
+          <label htmlFor="last_name">Last Name</label>
+          <input name="last_name" type="text" required="" placeholder="Doe" />
+          <br />
+          <label htmlFor="email">Email</label>
+          <input name="email" type="email" required="" placeholder="you@domain.com" />
+          <br />
+          <label htmlFor="message">Message</label>
+          <br />
+          <textarea
+            name="message"
+            cols={30}
+            rows={10}
+            placeholder="Enter your message here ..."
+            required=""
+            defaultValue={" "}
+          />
+          <div className="center">
+            <input type="submit" defaultValue="Submit" />
+          </div>
+        </form>
+      </div>
+      
     )
 }
 export default Contact;
